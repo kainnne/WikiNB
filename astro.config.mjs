40d330ed5c39cce5ebe4a90d61fc1b6f91f317f7
Kaine Zhu <chaos60649@gmail.com>
2026-08-10 16:16:32 +0800

    Configure WikiNB custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://zx50416.github.io',
-  base: '/WikiNB/',
+  site: 'https://wikinb.kainnne.com',
+  base: '/',
   integrations: [tailwind()],
   build: {
     // 內嵌樣式，避免部署後舊 HTML 指到已換 hash 的 .css 而整頁變素顏
